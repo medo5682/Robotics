1cf37e7bde1b9807fabd5ed7fb0993fa342b6e89
benjijamorris <54606172+benjijamorris@users.noreply.github.com>
2020-01-26 12:25:27 -0700

    everything works through turn 180

--- lab_1.ino
+++ lab_1.ino
@@ -1,25 +1,20 @@
 #include <Sparki.h>
 
-#define STATE_ROTATE
-#define STATE_CAPTURE
-#define STATE_DRIVE
-#define STATE_FOLLOW_LINE
-#define STATE_STOP_BEEP_DROP
-
-/* LAB STEPS 
-1. The robot rotates in place until it detects an object within 30cm with its ultrasound sensor.
-2. The robot then drives to within 7cm of the detected object and captures it in its gripper. 
-3. Once the object is secured, the robot turns 180 degrees and drives until it detects a line. 
-4. Once it has found a line, the robot will follow it until reaching the "start" marker 
-5. at which point it stops, beeps, and drops the carried object. */
+#define STATE_ROTATE 0
+#define STATE_DRIVE_AND_CAPTURE 1
+#define STATE_DRIVE 2
+#define STATE_TURN_180_DRIVE 3
+#define STATE_FOLLOW_LINE 4
+#define STATE_STOP_BEEP_DROP 5
+#define STATE_DONE 6
 
 // Set up some global variables with default values to be replaced during operation
 int current_state = STATE_ROTATE;
 const int threshold = 700; // IR reading threshold to detect whether there's a black line under the sensor
 int cm_distance = 1000;
-int line_left = 1000;
-int line_center = 1000;
-int line_right = 1000;
+int lineLeft = 1000;
+int lineCenter = 1000;
+int lineRight = 1000;
 
 
 void setup() {
@@ -29,85 +24,141 @@ void setup() {
   delay(1000); // Give the motor time to turn
   sparki.gripperOpen(); // Open the gripper
   delay(5000); // Give the motor time to open the griper
-  sparki.gripperStop(); // 5 seconds should be long enough
+//  sparki.gripperSt/op(); // 5 seconds should be long enough
   sparki.RGB(RGB_GREEN); // Change LED to green so we know the robot's setup is done!
 }
 
 void readSensors() {
   cm_distance= sparki.ping();
-  line_left= sparki.lineLeft();
-  line_right= sparki.lineRight();
-  line_center= sparki.lineCenter();
-  }
-
-
-void rotate(current_state) {  
-    sparki.moveRight();
-    if(sparki.ping() < 30){
-          sparki.moveStop();
-      }
+  lineLeft= sparki.lineLeft();
+  lineRight= sparki.lineRight();
+  lineCenter= sparki.lineCenter();
 }
 
-void drive_and_capture(current_state){
-  sparki.moveForward();
-  if(sparki.ping() < 7){
-    sparki.moveStop();
-    sparki.gripperClose();
-  }
-}
 
-void turn_180_drive(current_state) {
+void turn_180_drive(int current_state) {
   sparki.moveRight(180); // rotate right 180 degrees
-  if((line_center < threshold) && (line_left < threshold) && (line_right < threshold)){
+  if((lineCenter < threshold) && (lineLeft < threshold) && (lineRight < threshold)){
     sparki.moveForward();
   } else {
-    spark.moveStop();
+    sparki.moveStop();
   }
 }
 
-void line_to_start(current_state) {
-  break;
+void rotate() {  
+    sparki.moveRight();
+    if(cm_distance < 30 && cm_distance >0){
+          sparki.print("Object found at ");
+          sparki.print(cm_distance);
+          sparki.println("cm");
+          current_state = STATE_DRIVE_AND_CAPTURE;
+          sparki.print("CHANGING STATE TO DRIVE");
+          sparki.println(current_state);
+          sparki.updateLCD();
+          sparki.moveStop();
+          delay(1000);
+    }
 }
 
-void stop_beep_drop(current_state){
-  break;
+void drive_and_capture() { 
+    sparki.println("DRIVE CALLED"); 
+    sparki.updateLCD();
+    if (cm_distance > 7){
+      sparki.moveForward();  
+    }
+    else{
+      sparki.moveStop();
+      sparki.gripperClose();
+      current_state = STATE_TURN_180_DRIVE;
+      sparki.print("CHANGING STATE TO STATE_TURN_180_DRIVE");
+      sparki.println(current_state);
+      sparki.updateLCD();
+      delay(5000);
+    }
 }
 
-//void updateState(current_state) {
-//  //check if transition is met and return
-//  
-//}
+
+void turn_180_drive() {
+  sparki.moveRight(180); // rotate right 180 degrees
+  if((lineCenter < threshold) && (lineLeft < threshold) && (lineRight < threshold)){
+    sparki.moveForward();
+  } else {
+    current_state = STATE_FOLLOW_LINE;
+    sparki.moveStop();
+  }
+}
+
+
+void line_to_start() {
+  if ( lineLeft < threshold ) // if line is below left line sensor
+  {  
+    sparki.moveLeft(); // turn left
+  }
+ 
+  if ( lineRight < threshold ) // if line is below right line sensor
+  {  
+    sparki.moveRight(); // turn right
+  }
+ 
+  // if the center line sensor is the only one reading a line
+  if ( (lineCenter < threshold) && (lineLeft > threshold) && (lineRight > threshold) )
+  {
+    sparki.moveForward(); // move forward
+  }
+  else if(false){
+    current_state = STATE_STOP_BEEP_DROP;
+  }  
+  delay(100); // wait 0.1 seconds
+
+  
+  
+}
+
+void stop_beep_drop(){
+  sparki.moveStop();
+  delay(1000);
+  sparki.beep();
+  delay(1000);
+  sparki.gripperOpen();
+  delay(5000);  
+  current_state = STATE_DONE;
+}
+
+
 
 
 void loop() {
   // put your main code here, to run repeatedly:
   readSensors(); // Read sensors once per loop() call
 
-  sparki.clearLCD();
-  sparki.print("STATE: ");
-  sparki.println(current_state);
+//  sparki.clearLCD();
+//  sparki.print("STATE: ");/
+//  sparki.println(current_state);/
+//  sparki.updateLCD();/
 
-  
 
   // Your state machine code goes here
 
   switch(current_state){
     case STATE_ROTATE:
+      rotate();
       break;
-    case STATE_CAPTURE:
+    case STATE_DRIVE_AND_CAPTURE:
+      drive_and_capture();
       break;
-    case STATE_DRIVE:
+    case STATE_TURN_180_DRIVE:
+      turn_180_drive();
       break;
     case STATE_FOLLOW_LINE:
+      line_to_start();
       break;
     case STATE_STOP_BEEP_DROP:
+      stop_beep_drop();
       break;
     default:
       break;
   }
-
-  //current_state = updateState(current_state);
-
+  
   sparki.updateLCD();
   delay(100); // Only run controller at 10Hz
 }
