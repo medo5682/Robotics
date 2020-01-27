7437e95f5a8ea26bffc7150dfd213557180a3d83
benjijamorris <54606172+benjijamorris@users.noreply.github.com>
2020-01-27 14:19:03 -0700

    now sparki drives after turning 180 degrees
    but he still can't latch onto a line to follow and doesn't know when to drop the object.

--- lab_1.ino
+++ lab_1.ino
@@ -3,14 +3,14 @@
 #define STATE_ROTATE 0
 #define STATE_DRIVE_AND_CAPTURE 1
 #define STATE_DRIVE 2
-#define STATE_TURN_180_DRIVE 3
+#define STATE_TURN_180 3
 #define STATE_FOLLOW_LINE 4
 #define STATE_STOP_BEEP_DROP 5
 #define STATE_DONE 6
 
 // Set up some global variables with default values to be replaced during operation
 int current_state = STATE_ROTATE;
-const int threshold = 500; // IR reading threshold to detect whether there's a black line under the sensor
+const int threshold = 700; // IR reading threshold to detect whether there's a black line under the sensor
 int cm_distance = 1000;
 int lineLeft = 1000;
 int lineCenter = 1000;
@@ -47,7 +47,6 @@ void rotate() {
           sparki.println(current_state);
           sparki.updateLCD();
           sparki.moveStop();
-          delay(1000);
     }
 }
 
@@ -58,37 +57,51 @@ void drive_and_capture() {
     else{
       sparki.moveStop();
       sparki.gripperClose();
-      current_state = STATE_TURN_180_DRIVE;
+      current_state = STATE_TURN_180;
       sparki.println("CHANGING STATE TO");
       sparki.println(current_state);
       sparki.updateLCD();
       delay(5000);
+      sparki.gripperStop();
     }
 }
 
+void drive(){
+  if( (lineLeft > threshold) && (lineRight > threshold) ){
+    sparki.println("HAVENT FOUND THE LINE YET");
+    sparki.updateLCD();
+    sparki.moveForward();
+    current_state = STATE_DRIVE;
+  } 
+  else {
+    sparki.println("FOUND THE LINE");
+    current_state = STATE_FOLLOW_LINE;
+    sparki.updateLCD();
+    sparki.moveStop();
+  }
+  
+}
 
-void turn_180_drive() {
-//  sparki.println("TURN 180 and DRIVE CALLED");
-//  sparki.updateLCD();
-//  sparki.print(lineCenter);
-//  sparki.print(lineLeft);
-//  sparki.println(lineRight);
-//  sparki.updateLCD();
-//  sparki.moveRight(180); // rotate right 180 degrees
-//  delay(1000);
-//  
-//  if((lineCenter > threshold) && (lineLeft > threshold) && (lineRight > threshold)){
-//    sparki.moveForward();
-//  } else {
-//    current_state = STATE_FOLLOW_LINE;
-//    
-//    sparki.moveStop();
-//  }
+
+void turn_180() {
+  sparki.println("TURN 180 and DRIVE CALLED");
+  sparki.updateLCD();
+  sparki.moveRight(180); // rotate right 180 degrees
+  current_state= STATE_DRIVE;
 }
 
 
 void line_to_start() {
   sparki.println("LINE TO START");
+  sparki.println(lineLeft);
+  sparki.println(lineRight);
+  sparki.println(lineCenter);
+  sparki.updateLCD();
+  delay(1000);
+  if( (lineCenter <threshold) && (lineLeft <threshold) && (lineRight < threshold) ){
+    current_state = STATE_STOP_BEEP_DROP;
+  }  
+  
   if ( lineLeft < threshold ) // if line is below left line sensor
   {  
     sparki.moveLeft(); // turn left
@@ -102,20 +115,17 @@ void line_to_start() {
   // if the center line sensor is the only one reading a line
   if ( (lineCenter < threshold) && (lineLeft > threshold) && (lineRight > threshold) )
   {
+    
     sparki.moveForward(); // move forward
   }
-  else if(false){
-    current_state = STATE_STOP_BEEP_DROP;
-  }  
+  
   delay(100); // wait 0.1 seconds
 
-  
   
 }
 
 void stop_beep_drop(){
   sparki.moveStop();
-  delay(1000);
   sparki.beep();
   delay(1000);
   sparki.gripperOpen();
@@ -130,10 +140,10 @@ void loop() {
   // put your main code here, to run repeatedly:
   readSensors(); // Read sensors once per loop() call
 
-//  sparki.clearLCD();
-//  sparki.print("STATE: ");/
-//  sparki.println(current_state);/
-//  sparki.updateLCD();/
+  sparki.clearLCD();
+  sparki.print("STATE: ");
+  sparki.println(current_state);
+  sparki.updateLCD();
 
 
   // Your state machine code goes here
@@ -145,14 +155,17 @@ void loop() {
     case STATE_DRIVE_AND_CAPTURE:
       drive_and_capture();
       break;
-    case STATE_TURN_180_DRIVE:
-      turn_180_drive();
+    case STATE_TURN_180:
+      turn_180();
       break;
     case STATE_FOLLOW_LINE:
       line_to_start();
       break;
     case STATE_STOP_BEEP_DROP:
       stop_beep_drop();
+      break;
+   case STATE_DRIVE:
+      drive();
       break;
     default:
       break;
