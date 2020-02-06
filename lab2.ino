5bf8be0ce8ccb81713aabdc6583e6f104a250389
medo5682 <medo5682@colorado.edu>
2020-02-06 16:26:42 -0700

    update lab 2

--- lab2.ino
+++ lab2.ino
@@ -5,9 +5,14 @@
 // Program States
 #define CONTROLLER_FOLLOW_LINE 1
 #define CONTROLLER_DISTANCE_MEASURE 2
+#define MOVE_FORWARD 3
+#define MOVE_LEFT 4
+#define MOVE_RIGHT 5
+#define MOVE_STOP 6
 
 
 int current_state = CONTROLLER_DISTANCE_MEASURE; // Change this variable to determine which controller to run
+int type_move;
 const int threshold = 700;
 int line_left = 1000;
 int line_center = 1000;
@@ -15,6 +20,7 @@ int line_right = 1000;
 
 float pose_x = 0., pose_y = 0., pose_theta = 0.;
 float time_1, time_2, tot_time, time_start, time_end, final_time, tim, speed_30;
+float d_x, pr, r, wheel_sep_r, d_theta;
 
 void setup() {
   pose_x = 0.;
@@ -31,7 +37,6 @@ void readSensors() {
 }
 
 void measure_30cm_speed() {
-  // TODO
   if ((line_left < threshold) && (line_right < threshold) && (line_center < threshold)){
     sparki.moveStop();
     time_2 = (float)millis();
@@ -40,6 +45,7 @@ void measure_30cm_speed() {
     sparki.println(tot_time);
     sparki.updateLCD();
     current_state = CONTROLLER_FOLLOW_LINE;
+    type_move = MOVE_STOP;
     speed_30 = (.3/(tot_time/1000));
     sparki.print("speed: ");
     sparki.println(speed_30);
@@ -47,17 +53,87 @@ void measure_30cm_speed() {
     
   } else {
     sparki.moveForward();
+    type_move = MOVE_FORWARD;
   }
   
+}
+
+void lineFollow() {
+  int threshold = 500;
+
+  int lineLeft   = sparki.lineLeft();   // measure the left IR sensor
+  int lineCenter = sparki.lineCenter(); // measure the center IR sensor
+  int lineRight  = sparki.lineRight();  // measure the right IR sensor
+
+  if ( lineLeft < threshold ) // if line is below left line sensor
+  {  
+    sparki.moveLeft(); // turn left
+  }
+
+  if ( lineRight < threshold ) // if line is below right line sensor
+  {  
+    sparki.moveRight(); // turn right
+  }
+
+  // if the center line sensor is the only one reading a line
+  if ( (lineCenter < threshold) && (lineLeft > threshold) && (lineRight > threshold) )
+  {
+    sparki.moveForward(); // move forward
+  }  
+
+  sparki.clearLCD(); // wipe the screen
+
+  sparki.print("Line Left: "); // show left line sensor on screen
+  sparki.println(lineLeft);
+
+  sparki.print("Line Center: "); // show center line sensor on screen
+  sparki.println(lineCenter);
+
+  sparki.print("Line Right: "); // show right line sensor on screen
+  sparki.println(lineRight);
+
+  sparki.updateLCD(); // display all of the information written to the screen
+
+  delay(100); // wait 0.1 seconds
 }
 
 
 void updateOdometry() {
   // TODO
+  //speed_30 = v = r * theta_l/r
+  //xi = cos(theta) * (v) = v
+  //yi = sin(theta) * (v) = 0
+  //theta = (2v)/d
+  if (type_move == MOVE_FORWARD){
+    d_x = speed_30*.1;
+    pose_x = pose_x + d_x;
+  }
+  if (type_move == MOVE_RIGHT) {
+    //pr = 0.003;
+    //r = (8/3.1415926535);
+    wheel_sep_r = (8.7/2);
+    d_theta = (2*speed_30)/wheel_sep_r;
+    pose_theta = pose_theta + d_theta;
+  }
+  if (type_move == MOVE_LEFT) {
+    //pr = 0.003;
+    //r = (8/3.1415926535);
+    wheel_sep_r = (8.7/2);
+    d_theta = (2*speed_30)/wheel_sep_r;
+    pose_theta = pose_theta - d_theta;
+  }
+  displayOdometry();
 }
 
 void displayOdometry() {
-  // TODO
+  sparki.clearLCD();
+  sparki.print("pose x:");
+  sparki.println(pose_x);
+  sparki.print("pose y:");
+  sparki.println(pose_y);
+  sparki.print("pose theta:");
+  sparki.println(pose_theta);
+  sparki.updateLCD();
 }
 
 void loop() {
@@ -68,22 +144,18 @@ void loop() {
   
   switch (current_state) {
     case CONTROLLER_FOLLOW_LINE:
-      // TODO
+    sparki.moveForward(16);
+    type_move = MOVE_FORWARD;
+    //sparki.moveRight();
+    //type_move = MOVE_RIGHT;
+    updateOdometry();
       break;
     case CONTROLLER_DISTANCE_MEASURE:
       measure_30cm_speed();
-      break;
+      //break;
   }
   time_end = (float)millis();
   final_time = (time_end - time_start);
-  //sparki.clearLCD();
-//  sparki.print("final time: ");
-//  sparki.println(final_time);
-//  sparki.updateLCD();
 
   delay(1000*CYCLE_TIME - final_time);
-//  tim = millis();
-//  sparki.print("after cycle");
-//  sparki.println(tim-time_start);
-//  sparki.updateLCD();
 }
