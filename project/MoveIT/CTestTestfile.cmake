274b1c7c8cc4b4740e1065d050fb9e8afd4ece42
GayathriGude <gagu3776@colorado.edu>
2020-05-05 20:57:58 -0600

    moveiT

---
+++ project/MoveIT/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/gayathrigude/catkin_ws/src/MoveIT
+# Build directory: /home/gayathrigude/catkin_ws/build/MoveIT
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
